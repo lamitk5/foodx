dbaaa321b186d4806cc7175e6091217674740628
lamit <lamc6250@gmail.com>
2026-08-06 10:46:31 +0700

    cau truc du an

---
+++ src/main/java/com/nhom6/foodx/FoodxApplication.java
@@ -0,0 +1,13 @@
+package com.nhom6.foodx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodxApplication.class, args);
+    }
+
+}
